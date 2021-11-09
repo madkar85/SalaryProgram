969eafe6b070c46030a0d8fbf2991bf70def83dd
madkar85 <madeleine.karlsson@outlook.com>
2021-11-09 14:34:15 +0100

    Add project files.

---
+++ SalaryProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalaryProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
