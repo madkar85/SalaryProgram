bb89b0ca16c828df8a89aaedce6938f4300b8d24
madkar85 <madeleine.karlsson@outlook.com>
2021-11-10 21:25:22 +0100

    Added class to Controller-folder

---
+++ SalaryProgram/Controller/Information.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryProgram.Controller
+{
+    class Information
+    {
+        public void DisplayInfo()
+        {
+            //Skriv metod som visar användaren salary och role.
+            //Även alternativet att ta bort användaren?
+        }
+    }
+}
