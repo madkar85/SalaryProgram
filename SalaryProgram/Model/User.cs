20f6514c6dbc6d2392efcdab0acc5ae6e25c3134
madkar85 <madeleine.karlsson@outlook.com>
2021-11-10 21:17:08 +0100

    Added User and Admin info

--- SalaryProgram/Model/User.cs
+++ SalaryProgram/Model/User.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace SalaryProgram.Model
 {
-    class User
+    public class User: Account
     {
+        public User(string name, string password, int salary, string role)
+        {
+            this.Name = name;
+            this.Password = password;
+            this.IsAdmin = false;
+            this.Salary = salary;
+            this.Role = role;
+        }
+
+        public override void RemoveUser(string name, string password)
+        {
+            base.RemoveUser(name, password);
+        }
     }
 }
