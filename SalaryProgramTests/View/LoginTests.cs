c2dd35d072107e65cd7d6defe7d59142206cc3a1
madkar85 <madeleine.karlsson@outlook.com>
2021-11-17 10:11:53 +0100

    Added test-project

---
+++ SalaryProgramTests/View/LoginTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalaryProgram;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryProgram.Tests
+{
+    [TestClass()]
+    public class LoginTests
+    {
+        [TestMethod()]
+        public void StartTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
