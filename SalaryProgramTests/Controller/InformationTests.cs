82c017dfeb8c586fd1b48597886383051a6c20aa
madkar85 <madeleine.karlsson@outlook.com>
2021-11-20 15:58:30 +0100

    Changed test

---
+++ SalaryProgramTests/Controller/InformationTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalaryProgram.Controller;
+using SalaryProgram.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryProgram.Controller.Tests
+{
+    [TestClass()]
+    public class InformationTests
+    {
+        public static Dictionary<string, Account> allUsers;
+
+        [TestMethod()]
+        [DataRow("Madde","abc123")]
+        public void DisplayInfoTest(string name, string password)
+        {
+            allUsers = new Dictionary<string, Account>();
+            allUsers.Add("Madde", new User(name, password, 20000, "receptionist"));
+
+            var result = Information.DisplayInfo(name, allUsers);
+            var expected = true;
+
+            Assert.AreEqual(result, expected);
+        }
+
+    }
+}
